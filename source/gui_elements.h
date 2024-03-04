731d16a9bc33bdf10db7e4abb9a48354878d0af2
Joppy Furr <joppyfurr@gmail.com>
2024-03-04 14:43:30 +1300

    Pixel-accurate cursor size
    Previously, the position and size for the cursor rectangle was described
    using the number of 8x8 tiles. Now, the size is specified using the
    (x, y) pixel position and (w, h) opening size.

--- source/gui_elements.h
+++ source/gui_elements.h
@@ -46,8 +46,11 @@ typedef struct gui_element_s {
 
     uint8_t x;
     uint8_t y;
-    uint8_t width;
-    uint8_t height;
+
+    uint8_t cursor_x;
+    uint8_t cursor_y;
+    uint8_t cursor_w;
+    uint8_t cursor_h;
 
     element_id_t up;
     element_id_t down;
@@ -61,147 +64,147 @@ typedef struct gui_element_s {
 
 static const gui_element_t psg_gui [ELEMENT_COUNT] = {
     [ELEMENT_CH0_VOLUME] = {
-        .type = TYPE_VALUE, .max = 15,
-        .x = 5, .y = 3, .width = 2, .height = 3,
+        .type = TYPE_VALUE, .max = 15, .x = 5, .y = 3,
+        .cursor_x = 35, .cursor_y = 23, .cursor_w = 18, .cursor_h = 25,
         .callback = key_set_ch0_volume,
         .up   = ELEMENT_CH0_VOLUME,             .down  = ELEMENT_CH1_VOLUME,
         .left = ELEMENT_CH0_VOLUME,             .right = ELEMENT_CH0_MODE_KEYBOARD
     },
     [ELEMENT_CH0_MODE_KEYBOARD] = {
-        .type = TYPE_LED, .max = 1,
-        .x = 9, .y = 4, .width = 2, .height = 2,
+        .type = TYPE_LED, .max = 1, .x = 9, .y = 4,
+        .cursor_x = 59, .cursor_y = 31, .cursor_w = 34, .cursor_h = 16,
         .callback = key_set_ch0_mode_keyboard,
         .up   = ELEMENT_CH0_MODE_KEYBOARD,      .down  = ELEMENT_CH1_MODE_KEYBOARD,
         .left = ELEMENT_CH0_VOLUME,             .right = ELEMENT_CH0_MODE_CONSTANT
     },
     [ELEMENT_CH0_MODE_CONSTANT] = {
-        .type = TYPE_LED, .max = 1,
-        .x = 13, .y = 4, .width = 2, .height = 2,
+        .type = TYPE_LED, .max = 1, .x = 13, .y = 4,
+        .cursor_x = 97, .cursor_y = 31, .cursor_w = 22, .cursor_h = 16,
         .callback = key_set_ch0_mode_constant,
         .up   = ELEMENT_CH0_MODE_CONSTANT,      .down  = ELEMENT_CH1_MODE_CONSTANT,
         .left = ELEMENT_CH0_MODE_KEYBOARD,      .right = ELEMENT_CH0_FREQUENCY
     },
     [ELEMENT_CH0_FREQUENCY] = {
-        .type = TYPE_VALUE_WIDE, .max = 1023,
-        .x = 17, .y = 3, .width = 4, .height = 3,
+        .type = TYPE_VALUE_WIDE, .max = 1023, .x = 17, .y = 3,
+        .cursor_x = 129, .cursor_y = 23, .cursor_w = 38, .cursor_h = 25,
         .callback = key_set_ch0_frequency,
         .up   = ELEMENT_CH0_FREQUENCY,          .down  = ELEMENT_CH1_FREQUENCY,
         .left = ELEMENT_CH0_MODE_CONSTANT,      .right = ELEMENT_CH0_BUTTON
     },
     [ELEMENT_CH0_BUTTON] = {
-        .type = TYPE_BUTTON, .max = 1,
-        .x = 23, .y = 3, .width = 6, .height = 2,
+        .type = TYPE_BUTTON, .max = 1, .x = 23, .y = 3,
+        .cursor_x = 179, .cursor_y = 23, .cursor_w = 48, .cursor_h = 18,
         .callback = key_set_ch0_button,
         .up   = ELEMENT_CH0_BUTTON,             .down  = ELEMENT_CH1_BUTTON,
         .left = ELEMENT_CH0_FREQUENCY,          .right = ELEMENT_CH0_BUTTON
     },
     [ELEMENT_CH1_VOLUME] = {
-        .type = TYPE_VALUE, .max = 15,
-        .x = 5, .y = 6, .width = 2, .height = 3,
+        .type = TYPE_VALUE, .max = 15, .x = 5, .y = 6,
+        .cursor_x = 35, .cursor_y = 47, .cursor_w = 18, .cursor_h = 25,
         .callback = key_set_ch1_volume,
         .up   = ELEMENT_CH0_VOLUME,             .down  = ELEMENT_CH2_VOLUME,
         .left = ELEMENT_CH1_VOLUME,             .right = ELEMENT_CH1_MODE_KEYBOARD
     },
     [ELEMENT_CH1_MODE_KEYBOARD] = {
-        .type = TYPE_LED, .max = 1,
-        .x = 9, .y = 7, .width = 2, .height = 2,
+        .type = TYPE_LED, .max = 1, .x = 9, .y = 7,
+        .cursor_x = 59, .cursor_y = 55, .cursor_w = 34, .cursor_h = 16,
         .callback = key_set_ch1_mode_keyboard,
         .up   = ELEMENT_CH0_MODE_KEYBOARD,      .down  = ELEMENT_CH2_MODE_KEYBOARD,
         .left = ELEMENT_CH1_VOLUME,             .right = ELEMENT_CH1_MODE_CONSTANT
     },
     [ELEMENT_CH1_MODE_CONSTANT] = {
-        .type = TYPE_LED, .max = 1,
-        .x = 13, .y = 7, .width = 2, .height = 2,
+        .type = TYPE_LED, .max = 1, .x = 13, .y = 7,
+        .cursor_x = 97, .cursor_y = 55, .cursor_w = 22, .cursor_h = 16,
         .callback = key_set_ch1_mode_constant,
         .up   = ELEMENT_CH0_MODE_CONSTANT,      .down  = ELEMENT_CH2_MODE_CONSTANT,
         .left = ELEMENT_CH1_MODE_KEYBOARD,      .right = ELEMENT_CH1_FREQUENCY
     },
     [ELEMENT_CH1_FREQUENCY] = {
-        .type = TYPE_VALUE_WIDE, .max = 1023,
-        .x = 17, .y = 6, .width = 4, .height = 3,
+        .type = TYPE_VALUE_WIDE, .max = 1023, .x = 17, .y = 6,
+        .cursor_x = 129, .cursor_y = 47, .cursor_w = 38, .cursor_h = 25,
         .callback = key_set_ch1_frequency,
         .up   = ELEMENT_CH0_FREQUENCY,          .down  = ELEMENT_CH2_FREQUENCY,
         .left = ELEMENT_CH1_MODE_CONSTANT,      .right = ELEMENT_CH1_BUTTON
     },
     [ELEMENT_CH1_BUTTON] = {
-        .type = TYPE_BUTTON, .max = 1,
-        .x = 23, .y = 6, .width = 6, .height = 2,
+        .type = TYPE_BUTTON, .max = 1, .x = 23, .y = 6,
+        .cursor_x = 179, .cursor_y = 47, .cursor_w = 48, .cursor_h = 18,
         .callback = key_set_ch1_button,
         .up   = ELEMENT_CH0_BUTTON,             .down  = ELEMENT_CH2_BUTTON,
         .left = ELEMENT_CH1_FREQUENCY,          .right = ELEMENT_CH1_BUTTON
     },
     [ELEMENT_CH2_VOLUME] = {
-        .type = TYPE_VALUE, .max = 15,
-        .x = 5, .y = 9, .width = 2, .height = 3,
+        .type = TYPE_VALUE, .max = 15, .x = 5, .y = 9,
+        .cursor_x = 35, .cursor_y = 71, .cursor_w = 18, .cursor_h = 25,
         .callback = key_set_ch2_volume,
         .up   = ELEMENT_CH1_VOLUME,             .down  = ELEMENT_NOISE_VOLUME,
         .left = ELEMENT_CH2_VOLUME,             .right = ELEMENT_CH2_MODE_KEYBOARD
     },
     [ELEMENT_CH2_MODE_KEYBOARD] = {
-        .type = TYPE_LED, .max = 1,
-        .x = 9, .y = 10, .width = 2, .height = 2,
+        .type = TYPE_LED, .max = 1, .x = 9, .y = 10,
+        .cursor_x = 59, .cursor_y = 79, .cursor_w = 34, .cursor_h = 16,
         .callback = key_set_ch2_mode_keyboard,
         .up   = ELEMENT_CH1_MODE_KEYBOARD,      .down  = ELEMENT_NOISE_MODE_KEYBOARD,
         .left = ELEMENT_CH2_VOLUME,             .right = ELEMENT_CH2_MODE_CONSTANT
     },
     [ELEMENT_CH2_MODE_CONSTANT] = {
-        .type = TYPE_LED, .max = 1,
-        .x = 13, .y = 10, .width = 2, .height = 2,
+        .type = TYPE_LED, .max = 1, .x = 13, .y = 10,
+        .cursor_x = 97, .cursor_y = 79, .cursor_w = 22, .cursor_h = 16,
         .callback = key_set_ch2_mode_constant,
         .up   = ELEMENT_CH1_MODE_CONSTANT,      .down  = ELEMENT_NOISE_MODE_CONSTANT,
         .left = ELEMENT_CH2_MODE_KEYBOARD,      .right = ELEMENT_CH2_FREQUENCY
     },
     [ELEMENT_CH2_FREQUENCY] = {
-        .type = TYPE_VALUE_WIDE, .max = 1023,
-        .x = 17, .y = 9, .width = 4, .height = 3,
+        .type = TYPE_VALUE_WIDE, .max = 1023, .x = 17, .y = 9,
+        .cursor_x = 129, .cursor_y = 71, .cursor_w = 38, .cursor_h = 25,
         .callback = key_set_ch2_frequency,
         .up   = ELEMENT_CH1_FREQUENCY,          .down  = ELEMENT_NOISE_CONTROL,
         .left = ELEMENT_CH2_MODE_CONSTANT,      .right = ELEMENT_CH2_BUTTON
     },
     [ELEMENT_CH2_BUTTON] = {
-        .type = TYPE_BUTTON, .max = 1,
-        .x = 23, .y = 9, .width = 6, .height = 2,
+        .type = TYPE_BUTTON, .max = 1, .x = 23, .y = 9,
+        .cursor_x = 179, .cursor_y = 71, .cursor_w = 48, .cursor_h = 18,
         .callback = key_set_ch2_button,
         .up   = ELEMENT_CH1_BUTTON,             .down  = ELEMENT_NOISE_BUTTON,
         .left = ELEMENT_CH2_FREQUENCY,          .right = ELEMENT_CH2_BUTTON
     },
     [ELEMENT_NOISE_VOLUME] = {
-        .type = TYPE_VALUE, .max = 15,
-        .x = 5, .y = 12, .width = 2, .height = 3,
+        .type = TYPE_VALUE, .max = 15, .x = 5, .y = 12,
+        .cursor_x = 35, .cursor_y = 95, .cursor_w = 18, .cursor_h = 25,
         .callback = key_set_noise_volume,
         .up   = ELEMENT_CH2_VOLUME,             .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_NOISE_VOLUME,           .right = ELEMENT_NOISE_MODE_KEYBOARD
     },
     [ELEMENT_NOISE_MODE_KEYBOARD] = {
-        .type = TYPE_LED, .max = 1,
-        .x = 9, .y = 13, .width = 2, .height = 2,
+        .type = TYPE_LED, .max = 1, .x = 9, .y = 13,
+        .cursor_x = 59, .cursor_y = 103, .cursor_w = 34, .cursor_h = 16,
         .callback = key_set_noise_mode_keyboard,
         .up   = ELEMENT_CH2_MODE_KEYBOARD,      .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_NOISE_VOLUME,           .right = ELEMENT_NOISE_MODE_CONSTANT
     },
     [ELEMENT_NOISE_MODE_CONSTANT] = {
-        .type = TYPE_LED, .max = 1,
-        .x = 13, .y = 13, .width = 2, .height = 2,
+        .type = TYPE_LED, .max = 1, .x = 13, .y = 13,
+        .cursor_x = 97, .cursor_y = 103, .cursor_w = 22, .cursor_h = 16,
         .callback = key_set_noise_mode_constant,
         .up   = ELEMENT_CH2_MODE_CONSTANT,      .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_NOISE_MODE_KEYBOARD,    .right = ELEMENT_NOISE_CONTROL
     },
     [ELEMENT_NOISE_CONTROL] = {
-        .type = TYPE_VALUE, .max = 7,
-        .x = 18, .y = 12, .width = 2, .height = 3,
+        .type = TYPE_VALUE, .max = 7, .x = 18, .y = 12,
+        .cursor_x = 129, .cursor_y = 95, .cursor_w = 38, .cursor_h = 24,
         .callback = key_set_noise_control,
         .up   = ELEMENT_CH2_FREQUENCY,          .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_NOISE_MODE_CONSTANT,    .right = ELEMENT_NOISE_BUTTON
     },
     [ELEMENT_NOISE_BUTTON] = {
-        .type = TYPE_BUTTON, .max = 1,
-        .x = 23, .y = 12, .width = 6, .height = 2,
+        .type = TYPE_BUTTON, .max = 1, .x = 23, .y = 12,
+        .cursor_x = 179, .cursor_y = 95, .cursor_w = 48, .cursor_h = 18,
         .callback = key_set_noise_button,
         .up   = ELEMENT_CH2_BUTTON,             .down  = ELEMENT_KEYBOARD,
         .left = ELEMENT_NOISE_CONTROL,          .right = ELEMENT_NOISE_BUTTON
     },
     [ELEMENT_KEYBOARD] = {
-        .type = TYPE_KEYBOARD, .height = 0,
+        .type = TYPE_KEYBOARD, .cursor_w = 0,
         .down = ELEMENT_KEYBOARD,
         .left = ELEMENT_KEYBOARD,               .right = ELEMENT_KEYBOARD
     }
